ae477b392ae1dcd8e99f304d3cd3fbff57817463
jocelin79 <jocelinnunes@gmail.com>
2023-05-16 10:55:00 -0300

    up

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,23 @@
 const pokeApi = {}
 
+function converPokemonApiDetailToPokemon(pokeDatail){
+    const pokemon = new Pokemon()
+    pokemon.nome = pokeDetail.order
+    pokemon.name = pokeDatail.name
+
+    const types = pokeDatail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+}
+
 pokeApi.getPokemonsDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then((pokemon) => {
+
+            })
 }
 
 pokeApi.getPokemons = function(offset = 0, limit = 5) {
